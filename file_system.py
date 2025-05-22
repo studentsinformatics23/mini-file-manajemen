0dcbbafaed577d508a7b5137030b4d4077705d5c
studentsinformatics23 <154208839+studentsinformatics23@users.noreply.github.com>
2025-05-23 05:18:52 +0700

    penyesuaian file_system.py

--- file_system.py
+++ file_system.py
@@ -3,16 +3,84 @@ import json
 import os
 
 class MiniFileSystem:
-    def __init__(self, total_blocks=100):
-        self.disk = [None] * total_blocks
-        self.fs = {
-            'name' : '/',
-            'type' : 'dir',
-            'children' : {}
-        }
 
-        self.current_dir = self.fs
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 # Manipulasi File
     def create(self, filename):
         if filename in self.current_dir['children']:
