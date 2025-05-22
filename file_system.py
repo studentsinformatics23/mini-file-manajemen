440e9d6145c482af0aa09b8b8a4fbcb59556bffe
Khalisa Putri <237006117@student.unsil.ac.id>
2025-05-23 01:18:28 +0700

    penambahan fitur information file

--- file_system.py
+++ file_system.py
@@ -12,3 +12,24 @@ class MiniFileSystem:
         }
 
         self.current_dir = self.fs
+
+    def show_disk(self):
+            print("\nDisk Status (X = used, . = free):")
+            for i in range(0, len(self.disk), 10):
+                line = self.disk[i:i+10]
+                print(''.join(['X' if blk else '.' for blk in line]))
+
+    def show_metadata(self, filename):
+        if filename not in self.current_dir['children']:
+            return "File not found."
+        file = self.current_dir['children'][filename]
+        if file['type'] != 'file':
+            return f"'{filename}' is not a file."
+
+        return (
+            f"\nMetadata for '{filename}':\n"
+            f"  Start Block : {file.get('start_block')}\n"
+            f"  Size        : {file.get('size')} block(s)\n"
+            f"  Timestamp   : {file.get('timestamp')}\n"
+            f"  Content     : '{file.get('content')}'"
+    )
